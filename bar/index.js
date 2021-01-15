c2e6144d2d51a95f9eef0f2c43d460b635f08a54
taman9333 <taman9333@gmail.com>
2021-01-15 21:37:53 +0200

    Call bar service inside foo

--- bar/index.js
+++ bar/index.js
@@ -3,6 +3,7 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
+  console.log('Someone call Bar service')
   res.send('Hello from Bar service!')
 })
 
--- foo/index.js
+++ foo/index.js
@@ -1,8 +1,13 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const axios = require('axios')
 
-app.get('/', (req, res) => {
+app.get('/', async (req, res) => {
+  // calling Bar service & log response
+  const barRes = await axios.get("http://bar-svc:3000/")
+  console.log(`$$ ${JSON.stringify(barRes.data)}`)
+
   res.send('Hello from Foo service!')
 })
 
