4cbcefe4983ee0b5644818d9e0cf26763812d1d4
taman9333 <taman9333@gmail.com>
2021-01-17 03:55:12 +0200

    Extract headers from request & pass them down in order to jaeger to collect tracing data

--- foo/index.js
+++ foo/index.js
@@ -1,16 +1,38 @@
-const express = require('express')
-const app = express()
-const port = 3000
-const axios = require('axios')
+const express = require('express');
+const app = express();
+const port = 3000;
+const axios = require('axios');
 
 app.get('/', async (req, res) => {
   // calling Bar service & log response
-  const barRes = await axios.get("http://bar-svc:3000/")
-  console.log(`$$ ${JSON.stringify(barRes.data)}`)
+  // Forward Headers for tracing
+  const headers = traceHeaders(req);
 
-  res.send('Hello from Foo service!')
-})
+  console.log(`## ${JSON.stringify(headers)}`);
+  const barRes = await axios.get('http://bar-svc:3000/', { headers });
+
+  console.log(`$$ ${JSON.stringify(barRes.data)}`);
+
+  res.send('Hello from Foo service!');
+});
+
+function traceHeaders(req) {
+  incoming_headers = [
+    'x-request-id',
+    'x-b3-traceid',
+    'x-b3-spanid',
+    'x-b3-parentspanid',
+    'x-b3-sampled',
+    'x-b3-flags',
+    'x-ot-span-context'
+  ];
+  const headers = {};
+  for (let h of incoming_headers) {
+    if (req.header(h)) headers[h] = req.header(h);
+  }
+  return headers;
+}
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-})
+  console.log(`Example app listening at http://localhost:${port}`);
+});
