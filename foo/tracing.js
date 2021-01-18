286f1a324897d4847c180c8456d648b96aed6c70
taman9333 <taman9333@gmail.com>
2021-01-18 13:43:21 +0200

    temp2

--- foo/tracing.js
+++ foo/tracing.js
@@ -2,11 +2,35 @@
 
 const { LogLevel } = require('@opentelemetry/core');
 const { NodeTracerProvider } = require('@opentelemetry/node');
+const { SimpleSpanProcessor } = require('@opentelemetry/tracing');
+const { JaegerExporter } = require('@opentelemetry/exporter-jaeger');
 // const { B3Propagator } = require('@opentelemetry/core');
 
 const provider = new NodeTracerProvider({
+  plugins: {
+    express: {
+      enabled: true,
+      path: '@opentelemetry/plugin-express'
+    },
+    http: {
+      enabled: true,
+      path: '@opentelemetry/plugin-http'
+    }
+  },
   logLevel: LogLevel.ERROR
 });
+
+provider.addSpanProcessor(
+  new SimpleSpanProcessor(
+    new JaegerExporter({
+      serviceName: 'foo-started',
+      url: 'jaeger-collector.istio-system'
+      // If you are running your tracing backend on another host,
+      // you can point to it using the `url` parameter of the
+      // exporter config.
+    })
+  )
+);
 
 provider.register();
 // provider.register({ propagator: new B3Propagator() });
