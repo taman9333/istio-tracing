8e0b2d4aad1e424461bba02053b9d2317c2e58e9
taman9333 <taman9333@gmail.com>
2021-01-18 15:12:43 +0200

    Add opentelemtry for handling propagation

--- foo/index.js
+++ foo/index.js
@@ -6,32 +6,32 @@ const axios = require('axios');
 app.get('/', async (req, res) => {
   // calling Bar service & log response
   // Forward Headers for tracing
-  const headers = traceHeaders(req);
+  // const headers = traceHeaders(req);
 
-  console.log(`## ${JSON.stringify(headers)}`);
-  const barRes = await axios.get('http://bar-svc:3000/', { headers });
+  // console.log(`## ${JSON.stringify(headers)}`);
+  const barRes = await axios.get('http://bar-svc:3000/');
 
   // console.log(`$$ ${JSON.stringify(barRes.data)}`);
 
   res.send('Hello from Foo service!');
 });
 
-function traceHeaders(req) {
-  incoming_headers = [
-    'x-request-id',
-    'x-b3-traceid',
-    'x-b3-spanid',
-    'x-b3-parentspanid',
-    'x-b3-sampled',
-    'x-b3-flags',
-    'x-ot-span-context'
-  ];
-  const headers = {};
-  for (let h of incoming_headers) {
-    if (req.header(h)) headers[h] = req.header(h);
-  }
-  return headers;
-}
+// function traceHeaders(req) {
+//   incoming_headers = [
+//     'x-request-id',
+//     'x-b3-traceid',
+//     'x-b3-spanid',
+//     'x-b3-parentspanid',
+//     'x-b3-sampled',
+//     'x-b3-flags',
+//     'x-ot-span-context'
+//   ];
+//   const headers = {};
+//   for (let h of incoming_headers) {
+//     if (req.header(h)) headers[h] = req.header(h);
+//   }
+//   return headers;
+// }
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
