95f54d4021811e36ca0e91ffc5a3087aee8e70a2
taman9333 <taman9333@gmail.com>
2021-01-18 14:39:08 +0200

    successfully working

--- bar/tracing.js
+++ bar/tracing.js
@@ -1,36 +1,46 @@
 'use strict';
 
+// const { LogLevel } = require('@opentelemetry/core');
+// const { NodeTracerProvider } = require('@opentelemetry/node');
+// const { SimpleSpanProcessor } = require('@opentelemetry/tracing');
+// const { JaegerExporter } = require('@opentelemetry/exporter-jaeger');
+// const { B3Propagator } = require('@opentelemetry/propagator-b3');
+
+// const provider = new NodeTracerProvider({
+//   plugins: {
+//     express: {
+//       enabled: true,
+//       path: '@opentelemetry/plugin-express'
+//     },
+//     http: {
+//       enabled: true,
+//       path: '@opentelemetry/plugin-http'
+//     }
+//   },
+//   logLevel: LogLevel.ERROR
+// });
+
+// provider.addSpanProcessor(
+//   new SimpleSpanProcessor(
+//     new JaegerExporter({
+//       serviceName: 'bar-started'
+//       // url: 'jaeger-collector.istio-system'
+//       // If you are running your tracing backend on another host,
+//       // you can point to it using the `url` parameter of the
+//       // exporter config.
+//     })
+//   )
+// );
+
+// // provider.register();
+// provider.register({ propagator: new B3Propagator() });
+
 const { LogLevel } = require('@opentelemetry/core');
 const { NodeTracerProvider } = require('@opentelemetry/node');
-const { SimpleSpanProcessor } = require('@opentelemetry/tracing');
-const { JaegerExporter } = require('@opentelemetry/exporter-jaeger');
 const { B3Propagator } = require('@opentelemetry/propagator-b3');
 
 const provider = new NodeTracerProvider({
-  plugins: {
-    express: {
-      enabled: true,
-      path: '@opentelemetry/plugin-express'
-    },
-    http: {
-      enabled: true,
-      path: '@opentelemetry/plugin-http'
-    }
-  },
   logLevel: LogLevel.ERROR
 });
 
-provider.addSpanProcessor(
-  new SimpleSpanProcessor(
-    new JaegerExporter({
-      serviceName: 'bar-started'
-      // url: 'jaeger-collector.istio-system'
-      // If you are running your tracing backend on another host,
-      // you can point to it using the `url` parameter of the
-      // exporter config.
-    })
-  )
-);
-
-// provider.register();
 provider.register({ propagator: new B3Propagator() });
