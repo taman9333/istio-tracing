b653a4d64e105ef025da763e8c690179f64949a4
taman9333 <taman9333@gmail.com>
2021-01-20 02:00:03 +0200

    Temp multi-header as this is the one we need

--- bar/tracing.js
+++ bar/tracing.js
@@ -1,7 +1,11 @@
 const { LogLevel } = require('@opentelemetry/core');
 const { NodeTracerProvider } = require('@opentelemetry/node');
-// const api = require('@opentelemetry/api');
-const { B3Propagator } = require('@opentelemetry/propagator-b3');
+const api = require('@opentelemetry/api');
+const { SimpleSpanProcessor } = require('@opentelemetry/tracing');
+const { JaegerExporter } = require('@opentelemetry/exporter-jaeger');
+// const { B3Propagator } = require('@opentelemetry/propagator-b3');
+const { B3MultiPropagator } = require('@opentelemetry/propagator-b3');
+// const { B3SinglePropagator } = require('@opentelemetry/propagator-b3');
 
 const provider = new NodeTracerProvider({
   logLevel: LogLevel.ERROR
@@ -9,4 +13,19 @@ const provider = new NodeTracerProvider({
 
 // api.propagation.setGlobalPropagator(new B3Propagator());
 
-provider.register({ propagator: new B3Propagator() });
+api.propagation.setGlobalPropagator(new B3MultiPropagator());
+
+// api.propagation.setGlobalPropagator(new B3SinglePropagator());
+
+// provider.register({ propagator: new B3Propagator() });
+
+provider.register();
+
+// provider.addSpanProcessor(
+//   new SimpleSpanProcessor(
+//     new JaegerExporter({
+//       serviceName: 'bar',
+//       host: 'tracing.istio-system'
+//     })
+//   )
+// );
--- foo/index.js
+++ foo/index.js
@@ -11,6 +11,7 @@ app.get('/', async (req, res) => {
   // console.log(`## ${JSON.stringify(headers)}`);
   console.log(`## ${JSON.stringify(req.headers)}`);
   const barRes = await axios.get('http://bar-svc:3000/');
+  await axios.get('http://baz-svc:3000/');
 
   // console.log(`$$ ${JSON.stringify(barRes.data)}`);
 
